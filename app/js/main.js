eff3d4eb14b209e222941d2d9f75e32ec21ecab6
aleksandr.tloka <aleksandr.tloka@i.ua>
2023-03-29 21:32:33 +0300

    started section solutions

--- app/js/main.js
+++ app/js/main.js
@@ -660,9 +660,48 @@ __webpack_require__.r(__webpack_exports__);
 
 // Vertical slider for home page ========================================================================================================================================================
 
-if (document.querySelectorAll('.main-slider__wrap').length > 0) {
-  let anchors = ['Overview', 'Solutions', 'CF Leadership', 'CF Board', 'AMS', 'Ingeus', 'CF Sustainability', 'CF Diversity', 'CF Equity', 'CF Projects', 'EMR'];
+/* if (document.querySelectorAll('.main-slider__wrap').length > 0) {
+  let anchors = ['Overview', 'Solutions', 'CF Leadership', 'CF Board', 'AMS', 'Ingeus', 'CF Sustainability', 'CF Diversity', 'CF Equity', 'CF Projects', 'EMR' ];
+
   const swiper = new Swiper(".main-slider", {
+    direction: "vertical",
+    speed: 900,
+    effect: "fade",
+    fadeEffect: { crossFade: true },
+    slidesPerView: 1,
+    mousewheel: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      renderBullet: function (index, className) {
+        return '<span class="' + className + '">' + (anchors[index]) + "</span>";
+      },
+    },
+  });
+} */
+
+const breakpoint = window.matchMedia('(max-width: 768px)');
+
+// keep track of swiper instances to destroy later
+
+let mainSwiper;
+const breakpointChecker = function () {
+  // if larger viewport and multi-row layout needed
+  if (breakpoint.matches === true) {
+    // clean up old instances and inline styles when available
+
+    mainSwiper.destroy(true, true);
+
+    // or/and do nothing
+    // else if a small viewport and single column layout needed
+  } else if (breakpoint.matches === false) {
+    // fire small viewport version of swiper
+    return enableSwiper();
+  }
+};
+let enableSwiper = function () {
+  let anchors = ['Overview', 'Solutions', 'CF Leadership', 'CF Board', 'AMS', 'Ingeus', 'CF Sustainability', 'CF Diversity', 'CF Equity', 'CF Projects', 'EMR'];
+  mainSwiper = new Swiper(".main-slider", {
     direction: "vertical",
     speed: 900,
     effect: "fade",
@@ -679,7 +718,17 @@ if (document.querySelectorAll('.main-slider__wrap').length > 0) {
       }
     }
   });
-}
+};
+breakpoint.addListener(breakpointChecker);
+
+// kickstart
+
+document.addEventListener('DOMContentLoaded', () => {
+  breakpointChecker();
+});
+document.addEventListener('resize', () => {
+  breakpointChecker();
+});
 })();
 
 /******/ })()
--- src/js/main.js
+++ src/js/main.js
@@ -5,7 +5,7 @@ import './_components';
 
 // Vertical slider for home page ========================================================================================================================================================
 
-if (document.querySelectorAll('.main-slider__wrap').length > 0) {
+/* if (document.querySelectorAll('.main-slider__wrap').length > 0) {
   let anchors = ['Overview', 'Solutions', 'CF Leadership', 'CF Board', 'AMS', 'Ingeus', 'CF Sustainability', 'CF Diversity', 'CF Equity', 'CF Projects', 'EMR' ];
 
   const swiper = new Swiper(".main-slider", {
@@ -23,5 +23,63 @@ if (document.querySelectorAll('.main-slider__wrap').length > 0) {
       },
     },
   });
-}
+} */
+
+
+    const breakpoint = window.matchMedia('(max-width: 768px)');
+
+    // keep track of swiper instances to destroy later
+
+    let mainSwiper;
+    const breakpointChecker = function () {
+
+      // if larger viewport and multi-row layout needed
+      if (breakpoint.matches === true) {
+        // clean up old instances and inline styles when available
+
+        mainSwiper.destroy(true, true);
+
+        // or/and do nothing
+        // else if a small viewport and single column layout needed
+      } else if (breakpoint.matches === false) {
+        // fire small viewport version of swiper
+        return enableSwiper();
+
+      }
+
+      };
+
+      let enableSwiper = function () {
+
+        let anchors = ['Overview', 'Solutions', 'CF Leadership', 'CF Board', 'AMS', 'Ingeus', 'CF Sustainability', 'CF Diversity', 'CF Equity', 'CF Projects', 'EMR' ];
+
+        mainSwiper = new Swiper(".main-slider", {
+        direction: "vertical",
+        speed: 900,
+        effect: "fade",
+        fadeEffect: { crossFade: true },
+        slidesPerView: 1,
+        mousewheel: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+          renderBullet: function (index, className) {
+            return '<span class="' + className + '">' + (anchors[index]) + "</span>";
+          },
+        },
+      });
+    }
+
+
+    breakpoint.addListener(breakpointChecker);
+
+    // kickstart
+
+document.addEventListener('DOMContentLoaded', () => {
+  breakpointChecker();
+})
+document.addEventListener('resize', () => {
+  breakpointChecker();
+})
+
 
